1d1bec12d3d6624b8a1932ce95efa2b924ea9ccb
Mohamed Charfeddine <110181648+medcharfeddine@users.noreply.github.com>
2023-11-14 10:12:00 +0100

    Update client.js

--- src/client.js
+++ src/client.js
@@ -10,21 +10,25 @@ const client = [
                 "name": "BLOUSON SIMILI CUIR",
                 "image": "https://www.get.com.tn/images/produits/1697704922403.webp",
                 "price": "278.000TND",
+                "link": "https://www.get.com.tn/fr/produits/show/309/blouson-simili-cuir"
             },
             {
                 "name": "PANTALON SKINNY",
                 "image": "https://www.get.com.tn/images/produits/1684487430347.webp",
                 "price": "119.000TND",
+                "link": "https://www.get.com.tn/fr/produits/show/46/pantalon-skinny"
             },
             {
                 "name": "CHEMISE JEANS",
                 "image": "https://www.get.com.tn/images/produits/1698310903632.webp",
                 "price": "119.000TND",
+                "link": "https://www.get.com.tn/fr/produits/show/306/chemise-jeans"
             },
             {
                 "name": "Sweat À Capuche",
                 "image": "https://www.get.com.tn/images/produits/1697875646643.webp",
                 "price": "98.000TND",
+                "link": "https://www.get.com.tn/fr/produits/show/152/sweat-%C3%A0-capuche"
             },
         ]
     },
